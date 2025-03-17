d66d95b4c83d4d541735f90f34673dd43a59a5d4
Art Guerrero <artemio.guerreru@g.austincc.edu>
2025-03-17 14:03:38 -0500

    created a year variable,asked user for year,created an instance for the user input,use the instance to store the user input as the year using the year variable,check the year if it is a leap year,tell user if it is or isnt a leap year,if the year is divisible by 4 then,if the year is divivible by 400 then,if the year is divivible by 100 then

--- App.java
+++ App.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // create a year variable
+        int year;
+        // ask user for year
+        System.out.println("Please give us a year and we will tell you if it is a leap year: ");
+        // create an instance for the user input
+        Scanner sc = new Scanner(System.in);
+        // use the instance to store the user input as the year using the year variable
+        year = sc.nextInt();
+        // check the year if it is a leap year
+        // tell user if it is or isnt a leap year
+        if (year % 4 == 0) {
+            // if the year is divisible by 4 then
+            System.out.println("The year " + year + " is a leap year.");
+        } else if (year % 400 == 0) {
+            // if the year is divivible by 400 then
+            System.out.println("The year " + year + " is a leap year.");
+        } else if (year % 100 == 0) {
+            // if the year is divivible by 100 then
+            System.out.println("The year " + year + " is not a leap year.");
+        }
+        else {
+            // 
+            System.out.println("The year " + year + " is not a leap year.");
+        }
+        
+        // tell user if it is nonsense
+        
+        sc.close();
     }
 }
+
+
