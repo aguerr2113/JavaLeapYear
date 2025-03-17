5cc1a771022b8bf1a9a567100ce8c624f568e705
Art Guerrero <artemio.guerreru@g.austincc.edu>
2025-03-17 14:11:18 -0500

    inserted a while loop to be able to check if user is using a int or inputing the correct data format

--- App.java
+++ App.java
@@ -4,12 +4,32 @@ public class App {
     public static void main(String[] args) throws Exception {
         // create a year variable
         int year;
-        // ask user for year
-        System.out.println("Please give us a year and we will tell you if it is a leap year: ");
+
+        // create a boolean to check if user has entered a valid input
+        boolean validInput = false;
+
         // create an instance for the user input
         Scanner sc = new Scanner(System.in);
-        // use the instance to store the user input as the year using the year variable
-        year = sc.nextInt();
+
+        while (!validInput) {
+            // ask user for year
+            System.out.println("Please give us a year and we will tell you if it is a leap year: ");
+
+            // check if user enter an int
+            if (sc.hasNextInt()) {
+                // use the instance to store the user input as the year using the year variable
+                year = sc.nextInt();
+
+                validInput = true;
+                
+            }
+        }
+        
+
+
+
+        
+        
         // check the year if it is a leap year
         // tell user if it is or isnt a leap year
         if (year % 4 == 0) {
